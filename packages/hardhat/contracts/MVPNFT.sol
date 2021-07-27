2c4b6dabf2763d3aade42423bb123ee8eb34d2c5
mercury-protocol <84585728+MercuricChloride@users.noreply.github.com>
2021-07-27 09:56:12 -0400

    Changes
    -Removed the approvedList and replaced it with a single address.
    -Replaced some of the safeTransfer code with austin's work to prevent against reentrancy

--- packages/hardhat/contracts/MVPNFT.sol
+++ packages/hardhat/contracts/MVPNFT.sol
@@ -30,10 +30,10 @@ contract MVPNFT is ERC721, ERC165 {
     */
 
     //arbitrary string to be stored at the top of the contract
-    string public arbString = "Scaffold-eth RULES!";
+    string constant arbString = "Scaffold-eth RULES!";
+    uint constant tokenId = 1;
     address public owner; 
-    uint public tokenId = 1;
-    mapping (uint => address) public approvedList;
+    address public approved;
     //currently declaring the owner as my local acct on scaffold-eth
     constructor() {
         owner = 0xC9FFEe9e34723d882CB97a6c056100661d00Bfe1;
@@ -62,76 +62,73 @@ contract MVPNFT is ERC721, ERC165 {
         }
     }
 
+    //This is using austin's code from the TG to prevent reentrancy
     function safeTransferFrom(address _from, address _to, uint _tokenId, bytes memory data) external override payable {
-        require(msg.sender == owner || approvedList[tokenId] == msg.sender, "Msg.sender not allowed to transfer this NFT!");
+        require(msg.sender == owner || approved == msg.sender, "Msg.sender not allowed to transfer this NFT!");
         require(_from == owner && _from != address(0) && _tokenId == tokenId);
         if(isContract(_to)) {
-            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, data) == 0x150b7a02) {
-                emit Transfer(_from, _to, _tokenId);
-                approvedList[tokenId] = address(0);
-                owner = _to;
-            } else {
+            emit Transfer(_from, _to, _tokenId);
+            owner = _to;
+            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, data) != 0x150b7a02) {
                 revert("receiving address unable to hold ERC721!");
             }
         } else {
             emit Transfer(_from, _to, _tokenId);
-            approvedList[tokenId] = address(0);
+            approved = address(0);
             owner = _to;
         }
     }
 
+    //identical to the code above however this forces "" for the data field
     function safeTransferFrom(address _from, address _to, uint _tokenId) external override payable {
-        require(msg.sender == owner || approvedList[tokenId] == msg.sender, "Msg.sender not allowed to transfer this NFT!");
+        require(msg.sender == owner || approved == msg.sender, "Msg.sender not allowed to transfer this NFT!");
         require(_from == owner && _from != address(0) && _tokenId == tokenId);
         if(isContract(_to)) {
-            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, "") == 0x150b7a02) {
-                emit Transfer(_from, _to, _tokenId);
-                approvedList[tokenId] = address(0);
-                owner = _to;
-            }
-            else {
+            emit Transfer(_from, _to, _tokenId);
+            owner = _to;
+            if(ERC721TokenReceiver(_to).onERC721Received(msg.sender, _from, _tokenId, "") != 0x150b7a02) {
                 revert("receiving address unable to hold ERC721!");
             }
-        }
-        else {
+        } else {
             emit Transfer(_from, _to, _tokenId);
-            approvedList[tokenId] = address(0);
+            approved = address(0);
             owner = _to;
         }
+    }
     }
 
     function transferFrom(address _from, address _to, uint _tokenId) external override payable {
-        require(msg.sender == owner || approvedList[tokenId] == msg.sender, "Msg.sender not allowed to transfer this NFT!");
+        require(msg.sender == owner || approved == msg.sender, "Msg.sender not allowed to transfer this NFT!");
         require(_from == owner && _from != address(0) && _tokenId == tokenId);
         emit Transfer(_from, _to, _tokenId);
-        approvedList[tokenId] = address(0);
+        approved = address(0);
         owner = _to;
     }
 
     function approve(address _approved, uint256 _tokenId) external override payable {
-        require(msg.sender == owner || approvedList[tokenId] == msg.sender, "Msg.sender not allowed to approve");
+        require(msg.sender == owner || approved == msg.sender, "Msg.sender not allowed to approve");
         require(_tokenId == tokenId, "tokenId invald");
         emit Approval(owner, _approved, _tokenId);
-        approvedList[tokenId] == _approved;
+        approved = _approved;
     }
 
     function setApprovalForAll(address _operator, bool _approved) external override {
         require(msg.sender == owner, "Msg.sender not owner!");
         if (_approved) {
             emit ApprovalForAll(owner, _operator, _approved);
-            approvedList[tokenId] == _operator;
+            approved = _operator;
         } else {
             emit ApprovalForAll(owner, address(0), _approved);
-            approvedList[tokenId] = address(0);
+            approved = address(0);
         }
     }
 
     function getApproved(uint _tokenId) external view override returns (address) {
-        return approvedList[tokenId];
+        return approved;
     }
 
     function isApprovedForAll(address _owner, address _operator) external view override returns (bool) {
-        return approvedList[tokenId] == _operator;
+        return approved == _operator;
     }
 
     function supportsInterface(bytes4 interfaceID) external pure override returns (bool) {
